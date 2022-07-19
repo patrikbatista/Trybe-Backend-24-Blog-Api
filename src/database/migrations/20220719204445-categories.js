14917c2e12cdd008fb33267c3e0b71e1fe9ba33c
patrik <pcr_batista@hotmail.com>
2022-07-19 17:55:10 -0300

    feat: implementa migration de categories

--- src/database/migrations/20220719204445-categories.js
+++ src/database/migrations/20220719204445-categories.js
@@ -1,21 +1,20 @@
-'use strict';
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    const categoriesTable = queryInterface.createTable("Categories", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    });
+
+    return categoriesTable;
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  down: async (queryInterface) => queryInterface.dropTable("Categories"),
 };
