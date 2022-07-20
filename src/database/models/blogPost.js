ebf57d65d81ebc5f71774069c0ebb96b72e1d911
patrik <pcr_batista@hotmail.com>
2022-07-20 14:55:46 -0300

    feat: implementa novas propriedades

--- src/database/models/blogPost.js
+++ src/database/models/blogPost.js
@@ -1,9 +1,13 @@
 module.exports = (sequelize, DataTypes) => {
   const blogPostModel = sequelize.define('BlogPost', {
+
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     title: { type: DataTypes.STRING, allowNull: false },
     content: { type: DataTypes.STRING, allowNull: false },
     userId: { type: DataTypes.INTEGER, allowNull: false, foreignKey: true },
+    published: { type: DataTypes.DATE, allowNull: false},
+    updated: { type: DataTypes.DATE, allowNull: false}
+
   },
   {
     createdAt: 'published',
