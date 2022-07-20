5ef464bd558f4ef535c8106c04d630810b76ffd9
patrik <pcr_batista@hotmail.com>
2022-07-20 18:46:58 -0300

    feat: implementa controller de login

---
+++ src/controllers/login.js
@@ -0,0 +1,23 @@
+const userServices = require('../services/userServices');
+const createToken = require('../utils/createToken');
+
+const ERROR_UNDEFINED = {
+  status: 400,
+  message: 'Some required fields are missing',
+};
+
+const ERROR_INVALID_FIELDS = {
+  status: 400,
+  message: 'Invalid fields',
+};
+
+module.exports = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email && !password) throw ERROR_UNDEFINED;
+
+  const userExists = await userServices.loginUser(email, password);
+  if (!userExists) throw ERROR_INVALID_FIELDS;
+
+  const token = createToken(userExists.displayName, userExists.id);
+  return res.status(200).json({ token });
+};
