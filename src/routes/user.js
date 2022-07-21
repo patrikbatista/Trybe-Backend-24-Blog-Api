1463ef2c02a249c767fdc6d4c74d095d3d2f40be
patrik <pcr_batista@hotmail.com>
2022-07-21 13:00:20 -0300

    feat: implementa rota get users

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 
 const userController = require('../controllers/user');
+const tokenValidate = require('../controllers/tokenValidate');
 
 const user = Router();
 
 user.post('/', userController.createUser);
+user.get('/', tokenValidate, userController.getUsers);
 
 module.exports = user;
