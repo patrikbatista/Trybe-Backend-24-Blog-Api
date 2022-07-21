56195a3ccb7b64b6f7c10b43e0b6b4112061c073
patrik <pcr_batista@hotmail.com>
2022-07-21 11:52:48 -0300

    fix: corrige erro de user

--- src/api.js
+++ src/api.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/login', routes.login);
+app.use('/user', routes.user);
 app.use(handleError);
 
 // ...
---
+++ src/routes/user.js
@@ -0,0 +1,9 @@
+const { Router } = require('express');
+
+const userController = require('../controllers/user');
+
+const user = Router();
+
+user.post('/', userController.createUser);
+
+module.exports = user;
