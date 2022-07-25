c9bc3289c927d1642162b5b20c5e1617b428248c
patrik <pcr_batista@hotmail.com>
2022-07-25 14:07:57 -0300

    feat: implementa nova rota no index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const login = require('./login');
 const user = require('./user');
 const category = require('./category');
+const postCategory = require('./postCategory');
 
 module.exports = {
   login,
   user,
   category,
+  postCategory,
 };
